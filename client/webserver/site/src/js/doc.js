9783383f588acea6b8336d82057db56732ab5bb6
vctt <victor.duarte.oliveira@gmail.com>
2021-07-12 16:47:03 -0300

    changing hard coded string to locales

--- client/webserver/site/src/js/doc.js
+++ client/webserver/site/src/js/doc.js
@@ -1,3 +1,12 @@
+
+import Locales, {
+  ID_OFF,
+  ID_LOCKED,
+  ID_READY,
+  ID_NOWALLET,
+  ID_WALLET_SYNC_PROGRESS
+} from './locales'
+
 const parser = new window.DOMParser()
 
 const FPS = 30
@@ -238,7 +247,7 @@ export class WalletIcons {
     const i = this.icons
     Doc.hide(i.locked, i.unlocked, i.nowallet, i.syncing)
     Doc.show(i.sleeping)
-    if (this.status) this.status.textContent = 'off'
+    if (this.status) this.status.textContent = Locales.formatDetails(ID_OFF)
   }
 
   /*
@@ -248,7 +257,7 @@ export class WalletIcons {
     const i = this.icons
     Doc.hide(i.unlocked, i.nowallet, i.sleeping)
     Doc.show(i.locked)
-    if (this.status) this.status.textContent = 'locked'
+    if (this.status) this.status.textContent = Locales.formatDetails(ID_LOCKED)
   }
 
   /*
@@ -259,7 +268,7 @@ export class WalletIcons {
     const i = this.icons
     Doc.hide(i.locked, i.nowallet, i.sleeping)
     Doc.show(i.unlocked)
-    if (this.status) this.status.textContent = 'ready'
+    if (this.status) this.status.textContent = Locales.formatDetails(ID_READY)
   }
 
   /* sleeping sets the icons to indicate that no wallet exists. */
@@ -267,7 +276,7 @@ export class WalletIcons {
     const i = this.icons
     Doc.hide(i.locked, i.unlocked, i.sleeping, i.syncing)
     Doc.show(i.nowallet)
-    if (this.status) this.status.textContent = 'no wallet'
+    if (this.status) this.status.textContent = Locales.formatDetails(ID_NOWALLET)
   }
 
   setSyncing (wallet) {
@@ -278,7 +287,7 @@ export class WalletIcons {
     }
     if (!wallet.synced) {
       Doc.show(icon)
-      icon.dataset.tooltip = `wallet is ${(wallet.syncProgress * 100).toFixed(1)}% synced`
+      icon.dataset.tooltip = Locales.formatDetails(ID_WALLET_SYNC_PROGRESS, { syncProgress: (wallet.syncProgress * 100).toFixed(1) })
     } else Doc.hide(icon)
   }
 
--- client/webserver/site/src/js/locales.js
+++ client/webserver/site/src/js/locales.js
@@ -1,41 +1,84 @@
-export const LOGIN_ERROR_MESSAGE = 'LOGIN_ERROR_MESSAGE'
-export const SET_BUTTON_BUY = 'SET_BUTTON_BUY'
+export const ID_NO_PASS_ERROR_MSG = 'ID_NO_PASS_ERROR_MSG'
+export const ID_NO_APP_PASS_ERROR_MSG = 'ID_NO_APP_PASS_ERROR_MSG'
+export const ID_SET_BUTTON_BUY = 'ID_SET_BUTTON_BUY'
+export const ID_SET_BUTTON_SELL = 'ID_SET_BUTTON_SELL'
+export const ID_OFF = 'ID_OFF'
+export const ID_READY = 'ID_READY'
+export const ID_LOCKED = 'ID_LOCKED'
+export const ID_NOWALLET = 'ID_NOWALLET'
+export const ID_WALLET_SYNC_PROGRESS = 'ID_WALLET_SYNC_PROGRESS'
+export const ID_HIDE_ADDIIONAL_SETTINGS = 'ID_HIDE_ADDIIONAL_SETTINGS'
+export const ID_SHOW_ADDIIONAL_SETTINGS = 'ID_SHOW_ADDIIONAL_SETTINGS'
+export const ID_BUY = 'ID_BUY'
+export const ID_SELL = 'ID_SELL'
+export const ID_NOT_SUPPORTED = 'ID_NOT_SUPPORTED'
+export const ID_CONNECTION_FAILED = 'ID_CONNECTION_FAILED'
+export const ID_ORDER_PREVIEW = 'ID_ORDER_PREVIEW'
+export const ID_CALCULATING = 'ID_CALCULATING'
+export const ID_ESTIMATE_UNAVAILABLE = 'ID_ESTIMATE_UNAVAILABLE'
+export const ID_NO_ZERO_RATE = 'ID_NO_ZERO_RATE'
+export const ID_NO_ZERO_QUANTITY = 'ID_NO_ZERO_QUANTITY'
+export const ID_TRADE = 'ID_TRADE'
+export const ID_NO_ASSET_WALLET = 'ID_NO_ASSET_WALLET'
+export const ID_EXECUTED = 'ID_EXECUTED'
+export const ID_BOOKED = 'ID_BOOKED'
 
 export const templateKeys = {
-  [LOGIN_ERROR_MESSAGE]: 'password cannot be empty',
-  [SET_BUTTON_BUY]: `Place order to buy  {{ asset }}`
-
-};
+  [ID_NO_PASS_ERROR_MSG]: 'password cannot be empty',
+  [ID_NO_APP_PASS_ERROR_MSG]: 'app password cannot be empty',
+  [ID_SET_BUTTON_BUY]: 'Place order to buy  {{ asset }}',
+  [ID_SET_BUTTON_SELL]: 'Place order to sell {{ asset }}',
+  [ID_OFF]: 'off',
+  [ID_READY]: 'ready',
+  [ID_LOCKED]: 'locked',
+  [ID_NOWALLET]: 'no wallet',
+  [ID_WALLET_SYNC_PROGRESS]: 'wallet is {{ syncProgress }}% synced',
+  [ID_HIDE_ADDIIONAL_SETTINGS]: 'hide additional settings',
+  [ID_SHOW_ADDIIONAL_SETTINGS]: 'show additional settings',
+  [ID_BUY]: 'Buy',
+  [ID_SELL]: 'Sell',
+  [ID_NOT_SUPPORTED]: '{{ asset }} is not supported',
+  [ID_CONNECTION_FAILED]: 'Connection to dex server failed. You can close dexc and try again later or wait for it to reconnect.',
+  [ID_ORDER_PREVIEW]: 'Total: {{ total }} {{ asset }}',
+  [ID_CALCULATING]: 'calculating...',
+  [ID_ESTIMATE_UNAVAILABLE]: 'estimate unavailable',
+  [ID_NO_ZERO_RATE]: 'zero rate not allowed',
+  [ID_NO_ZERO_QUANTITY]: 'zero quantity not allowed',
+  [ID_TRADE]: 'trade',
+  [ID_NO_ASSET_WALLET]: 'No {{ asset }} wallet',
+  [ID_EXECUTED]: 'executed',
+  [ID_BOOKED]: 'booked'
+}
 
 export default class Locales {
-  static getCurrentLocale() {
+  static getCurrentLocale () {
     return window.navigator.userLanguage || window.navigator.language
   }
 
   // get from webserver/locales, but how?
-  getLocales() {
+  getLocales () {
   }
 
-  setLocale(locale) {
+  setLocale (locale) {
     // check if setted locale is available on locales.
     // if (this.getLocales().indexOf(locale) !== -1) {
     //   ...
 
   }
- 
+
   // formatDetails will format the message to its locale.
   // need to add one for plurals
-  static formatDetails(stringKey, args = undefined) {
+  static formatDetails (stringKey, args = undefined) {
     return this.stringTemplateParser(templateKeys[stringKey], args)
   }
 
   // reference: https://stackoverflow.com/questions/43261798/javascript-how-to-use-template-literals-with-json
-  static stringTemplateParser(expression, valueObj) {
-    const templateMatcher = /{{\s?([^{}\s]*)\s?}}/g;
-    let text = expression.replace(templateMatcher, (substring, value, index) => {
-      value = valueObj[value];
-      return value;
-    });
+  static stringTemplateParser (expression, valueObj) {
+    const templateMatcher = /{{\s?([^{}\s]*)\s?}}/g
+    const text = expression.replace(templateMatcher, (substring, value, index) => {
+      value = valueObj[value]
+      return value
+    })
     return text
   }
 }
